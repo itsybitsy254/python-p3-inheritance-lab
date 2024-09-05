c70ed12db73d7a63ddd5d228d303157d722d9680
Vitalis Ian <viterlis254@gmail.com>
2024-09-05 11:06:35 +0300

    initial

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
+# teacher.py
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Python programming",
+            "Data structures",
+            "Algorithms",
+            "Machine learning",
+            "Artificial intelligence",
+            "Computer networks",
+            "Cybersecurity",
+            "Database management"
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
